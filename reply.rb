60d310fd68993c790444400fb8088ec5a7663f3c
RJ D. and Aiven S <rj+aiven@gmail.com>
2015-08-11 11:55:41 -0400

    Finishes Reply class.

--- reply.rb
+++ reply.rb
@@ -15,6 +15,45 @@ class Reply
     Reply.new(reply)
   end
 
+  def self.find_by_user(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        replies.user_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
+  def self.find_by_question(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        replies.question_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
+  def self.find_by_parent_reply(parent_reply_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, parent_reply_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        replies.parent_reply_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
   attr_accessor :body
   attr_reader :id, :user_id, :question_id, :parent_reply_id
 
